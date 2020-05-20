64d0fa06d00f2cf4f642451c087b5c61e2f35314
davigrca <davigrca@gmail.com>
2020-05-19 21:43:49 -0300

    fixed up paths

--- src/index.js
+++ src/index.js
@@ -11,4 +11,8 @@ app.get('/how-to-use', (req, res) => {
   res.sendFile(path.join(__dirname + '/how-to-use.html'));
 });
 
+app.get('/info', (req, res) => {
+  res.sendFile(path.join(__dirname + '/info.html'));
+});
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
