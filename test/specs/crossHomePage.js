c437b5e79aeb86ca9708744803afd55e179a0b15
davigrca <davigrca@gmail.com>
2020-05-25 02:01:52 -0300

    added new test cases for add and remove buttons

--- test/specs/crossHomePage.js
+++ test/specs/crossHomePage.js
@@ -18,78 +18,78 @@ class crossHomePage{
     browser.url('/');
   }
 
-//Get a single element by the key from SELECTORS
-getElement(selectorName) {
-  return browser.$(SELECTORS[selectorName]);
-}
-
-//Get all elements in an array by the key from SELECTORS
-getElements(selectorName){
-  return browser.$$(SELECTORS[selectorName]);
-}
-
-//Get the song element at position (starts at 0)
-getSongAtIndex(index) {
-  return this.getElements('addedSongs')[index];
-}
-
-clickLogoButton(){
-  this.getElement('logoButton').click();
-}
-
-clickHowToUseButton(){
-  this.getElement('howToButton').click();
-}
-
-clickInfoButton(){
-  this.getElement('infoButton').click();
-}
-
-addSong(youtubeID, start, end){
-  const songInput = this.getElement('idInput');
-  songInput.addValue(youtubeID);
-
-  const startTime = this.getElement('startInput');
-  startTime.addValue(start);
-
-  const endTime = this.getElement('endInput');
-  endTime.addValue(end);
-
-  this.getElement('addSongButton').click();
-}
-
-//set index array a default parameter = 0, since 0 is the first song
-removeSong(songIndexes = [0]){
-  if (typeof(songIndexes) == 'number'){
-    songIndexes = [songIndexes]
+  //Get a single element by the key from SELECTORS
+  getElement(selectorName) {
+    return browser.$(SELECTORS[selectorName]);
   }
 
-  const myObj = this;
+  //Get all elements in an array by the key from SELECTORS
+  getElements(selectorName){
+    return browser.$$(SELECTORS[selectorName]);
+  }
 
-  songIndexes.forEach(function(index){
-    myObj.getSongAtIndex(index).click();
-  });
+  //Get the song element at position (starts at 0)
+  getSongAtIndex(index) {
+    return this.getElements('addedSongs')[index];
+  }
 
-  this.getElement('removeButton').click();
-}
+  clickLogoButton(){
+    this.getElement('logoButton').click();
+  }
+
+  clickHowToUseButton(){
+    this.getElement('howToButton').click();
+  }
+
+  clickInfoButton(){
+    this.getElement('infoButton').click();
+  }
+
+  addSong(youtubeID, start, end){
+    const songInput = this.getElement('idInput');
+    songInput.addValue(youtubeID);
+
+    const startTime = this.getElement('startInput');
+    startTime.addValue(start);
+
+    const endTime = this.getElement('endInput');
+    endTime.addValue(end);
+
+    this.getElement('addSongButton').click();
+  }
+
+  //set index array a default parameter = 0, since 0 is the first song
+  removeSong(songIndexes = [0]){
+    if (typeof(songIndexes) == 'number'){
+      songIndexes = [songIndexes]
+    }
+
+    const myObj = this;
+
+    songIndexes.forEach(function(index){
+      myObj.getSongAtIndex(index).click();
+    });
+
+    this.getElement('removeButton').click();
+  }
 
 
-songIndex(index){
-  //get a song(all details) at position(index) e.g. song #5
-  const songElement = this.getSongAtIndex(index);
+  songIndex(index){
+    //get a song(all details) at position(index) e.g. song #5
+    const songElement = this.getSongAtIndex(index);
 
-  //search only in children of songElement for the element with the song number (#5)
-  const songIndex = songElement.$(SELECTORS['songNumberInList']);
+    //search only in children of songElement for the element with the song number (#5)
+    const songIndex = songElement.$(SELECTORS['songNumberInList']);
 
-  //return the text with the number e.g. '5'
-  return songIndex.getText();
-}
+    //return the text with the number e.g. '5'
+    return songIndex.getText();
+  }
 
-getID(index){
-  const songElement = this.getSongAtIndex(index);
-  const song_ID = songElement.$(SELECTORS['songID']);
-  return song_ID.getText();
-}
+  getID(index){
+    const songElement = this.getSongAtIndex(index);
+    const song_ID = songElement.$(SELECTORS['songID']);
+    return song_ID.getText();
+  }
 
 }
 
